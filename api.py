d0009314191e5f0d3273e854b53fd7f41bd8d1b9
Asutosh-1234 <your-email@example.com>
2025-02-26 13:12:36 +0530

    Initial commit

--- api.py
+++ api.py
@@ -1,1 +1,5 @@
-print("asdbdba")
+a=int (input("Enter any one option : \n1 : Intrested\n2 : Not intrested"))
+if not a :
+    print ("Name : Ashutosh \nThank you")
+else:
+    print("Thank you for coming")
